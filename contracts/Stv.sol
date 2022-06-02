600f900db806f6e3c0ffa71bf855925e6466f64e
s29 0GNakamura <nots29vv@gmail.com>
2022-06-02 18:15:39 +0100

    Update to vote logic

--- contracts/Stv.sol
+++ contracts/Stv.sol
@@ -1,26 +1,47 @@
 
 pragma solidity ^0.7.0;
-
+import "hardhat/console.sol";
 
 
 contract Stv {   
-    event BallotCreated(string voteName, uint8 noOfProposals);
+    event    BallotCreated (string voteName, uint8 noOfProposals);
+    event    UserSuccessfullyVoted (string success, uint256[] preference);
 
-    string         public VoteName;
-    bool           public isBallotCreated;
-
-    uint immutable public Date;
-    uint immutable public Duration;
-    
+    modifier isVoteValid(uint256[] calldata preferenceRank)   {
+                    require(preferenceRank.length == Proposals.length, "Vote exceeds the number of proposals");
+                    for (uint8 start  = 0; start  < preferenceRank.length; start++)    {
+                        if (preferenceRank[start] > Proposals.length)            {
+                                    revert("Rank cannot be higher than number of representative");
+                        }
+                        if (preferenceRank[start] == 0)            {
+                                    revert("Rank cannot be equal to zero");
+                        }
+                    }
+                    _;
+    } 
 
     // Proposal == representative
+    // might remove this
     struct Proposal {
            bytes32 name;
     }
 
-    Proposal[] public Proposals;
+    string         public  VoteName;
+    bool           public  isBallotCreated;
+    uint immutable public  Date;
+    uint immutable public  Duration;
+    Proposal[ ]    public  Proposals;
 
-    mapping(uint8 => bytes32) public Ballot;
+
+
+    // think of it as the Ballot Paper;
+    mapping (uint8 => mapping(bytes32 => uint256))     public Ballot;
+
+    // might remove the struct above for this, this helps track a voters alternative choice
+    // e.g what percentage of people voted this proposal as second choice
+    mapping (uint8 => mapping(bytes32 => uint256))     public _Proposal;
+
+
 
     constructor(string memory _voteName, uint _date, uint _duration, bytes32[] memory _proposalNames) {
                 VoteName  = _voteName; 
@@ -33,37 +54,59 @@ contract Stv {
                 }
     }
 
-    function     _createBallot()               private                            {
+    function     _createBallot()                       private                            {
                 require(!isBallotCreated, "Ballot has already been created");
                 uint8 noOfProposals;
                 for (uint8 start  = 0; start < Proposals.length; start++)    {
-                        Ballot[start] = Proposals[start].name;
+                        Ballot[start][Proposals[start].name] = 0;
                         noOfProposals += 1;
                 }
+                
                 emit    BallotCreated(VoteName, noOfProposals);
     }
 
-    function     createBallot()                 external                           {
+    function     createBallot()                        external                                        {
                  _createBallot();
                  isBallotCreated = true;
     }
-    // comment out vote function and you can run a test
-    function     vote(uint8[] preferenceRank) external                             {
-                 if  (isVoteValid(preferenceRank)) {
-                  // would continue in the morning
-                 };
-             
+    // To get total ballot count loop round getProposalCount(uint8 _proposalNo) by the number of proposals.
+    
+    function     getProposalCount(uint8 _proposalNo)   public      view      returns (uint256)         {
+                 uint256 numerator = Ballot[_proposalNo][Proposals[_proposalNo].name] * 10 ** 18;
+                 uint256 denominator = Ballot[6]["Perfect_Vote"];
+                 return  numerator / denominator;
+    }
+
+    function     ProposalThreshold()                   public      view      returns (uint256)         {
+                 uint256   numerator   =  100 * 10 ** 18;
+                 uint256   denominator =  Proposals.length * 100;
+                 return    numerator   /  denominator;
+    }
+
+    // To-Do modify this function to only be called after votes are done
+    function     isProposalAboveThreshold(uint8 _proposalNo) public      view      returns (bool)         {    
+                if (getProposalCount(_proposalNo) >=  ProposalThreshold()) {
+                         return true;
+                } else {
+                         return false;  
+                }         
+    }
+
+    // To-do: only a valid user can call vote, and only once can he call vote
+    function     vote(uint256[] calldata preferenceRank) external  isVoteValid(preferenceRank)         {
+                        uint256 numerator;
+                        uint256 denominator;
+                 for (uint8 start  = 0; start < Proposals.length; start++)    {
+                        numerator = 100 * 10 ** 18;
+                        denominator = preferenceRank[start];
+                        Ballot[start][Proposals[start].name] += numerator / denominator;
+                 }
+                        Ballot[6]["Perfect_Vote"] += 100 * 10 ** 18;
+
+                 emit UserSuccessfullyVoted("You've successfully voted", preferenceRank);
     }  
 
-    function isVoteValid(uint8[] memory preferenceRank)    public    returns    (bool)    {
-                require(preferenceRank.length == Proposals.length, "Vote exceeds the number of proposals");
-                for (uint8 start  = 0; start  < preferenceRank.length; start++)    {
-                    if (preferenceRank[start] > Proposals.length)            {
-                                  return false;
-                    }
-                }
-                return true;
-    } 
+
 
     
 
--- test/Stv.js
+++ test/Stv.js
@@ -21,15 +21,15 @@ describe("Stv Factory Contract", function () {
     const voteName   = "Vote for Budget";
     let proposal_one = ethers.utils.formatBytes32String("Budget A: $50000")
     let proposal_two = ethers.utils.formatBytes32String("Budget B: $25000")
-    proposals        = [proposal_one, proposal_two]
+    let proposal_three = ethers.utils.formatBytes32String("Budget B: $15000")
+    let proposal_four = ethers.utils.formatBytes32String("Budget B: $2500")
+    proposals        = [proposal_one, proposal_two, proposal_three, proposal_four]
     const date       =  90; // dummy
     const duration   =  92; // dummy
  
     // deploy an Stv contract
     const deploy = await hardhatSTVFactory.deploy(voteName, date, duration, proposals)
-    const receipt = await deploy.wait(
-
-    )
+    const receipt = await deploy.wait()
     Stv_address = receipt.events[0].args[0];
     console.log("Deployed Stv address:",receipt.events[0].args[0])
   });
@@ -40,14 +40,68 @@ describe("Stv Factory Contract", function () {
     await   hardhatStv.createBallot();
     const   isBallotCreated = await hardhatStv.isBallotCreated()
     if      (isBallotCreated)  {  console.log("Ballot has been created")  }
-    for (let start = 0; start < proposals.length; start++) {
-             const ballot_proposal = await hardhatStv.Ballot(start);
-             let   originalText    = ethers.utils.parseBytes32String(ballot_proposal);
-             console.log(`Ballot Proposal ${start}: ${originalText}`)
-    }
     // await hardhatStv.createBallot() // call again to confirm ballot can only be created once
   });
+
+  it("votes on a ballot in the deployed Stv contract", async function () {
+    const   Stv             = await ethers.getContractFactory("Stv");
+    const   hardhatStv      = await Stv.attach(Stv_address);
+    // users vote by preference
+    let user_one_vote = [4,3,2,1];
+    let user_two_vote = [1,2,3,4];
+    let user_three_vote = [3,1,4,2];
+    let user_four_vote = [2,1,3,4];
+    //  
+    let users_vote = [user_one_vote, user_two_vote, user_three_vote, user_four_vote]
+    // vote 
+    for (let start = 0; start < users_vote.length; start++) {
+            let vote = await hardhatStv.vote(users_vote[start]);
+            // events emitted
+            let receipt = await vote.wait() 
+            console.log(`${receipt.events[0].args[0]}; this were your preference ${receipt.events[0].args[1]};`)
+    }
+
+    // this votes should fail
+    let user_Test_vote1 = [4,0,2,1]; // should fail if users votes preference of zero
+    let user_Test_vote2 = [4,3,6,1]; // should fail if users votes preference above the no of proposal or representatives
+    let unsuccessful_votes = [ user_Test_vote2, user_Test_vote1] 
+    for (let start = 0; start < unsuccessful_votes.length; start++) {
+      let vote = await hardhatStv.vote(unsuccessful_votes[start]);
+      // events emitted
+      let receipt = await vote.wait() 
+      console.log(`${receipt.events[0].args[0]}; this were your preference ${receipt.events[0].args[1]};`)
+    }
+  });
+
+  it("gets the Ballot count after the vote take place", async function () {
+    const   Stv             = await ethers.getContractFactory("Stv");
+    const   hardhatStv      = await Stv.attach(Stv_address);
+    for (let start = 0; start < proposals.length; start++) {
+            let   proposalCount   = await hardhatStv.getProposalCount(start)
+            let   originalText    = ethers.utils.parseBytes32String(proposals[start]);
+             console.log(`Ballot Proposal count ${start}. ${originalText} voteCount ${proposalCount}`)
+    }
+  }); 
+      
+
+  it("validates if a proposal is above the proposal threshold", async function () {
+    const   Stv             = await ethers.getContractFactory("Stv");
+    const   hardhatStv      = await Stv.attach(Stv_address);
+    // check for proposal threshold
+    let     Threshold       = await hardhatStv.ProposalThreshold()
+    console.log(`Ballot Proposal Threshold: ${Threshold}`)
+    for (let start = 0; start < proposals.length; start++) {
+            let   isAboveThreshold   = await hardhatStv.isProposalAboveThreshold(start)
+            let   originalText    = ethers.utils.parseBytes32String(proposals[start]);
+             console.log(`Ballot Proposal ${start}. ${originalText} isAboveThreshold: ${isAboveThreshold}`)
+    }
+  });
 });
+// 1 000 000 000 000 000 000
+// 416666666666666666
+// 416666666666666666
+// 250000000000000000
+// 250000000000000000
 
 
 
